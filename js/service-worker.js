ffcade2bf299876b0081c561dbec3feee721be2f
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-11-22 22:52:26 -0300

    create the offiline page and the service-worker file

---
+++ js/service-worker.js
@@ -0,0 +1,56 @@
+// service-worker.js
+
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "css/index.css",
+  "js/index.js",
+  "offline.html", // Adicione a página offline ao cache
+  // Adicione outros recursos que você deseja armazenar em cache
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          // Se a solicitação falhar, retorna a página offline
+          return caches.match("offline.html");
+        }
+
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
